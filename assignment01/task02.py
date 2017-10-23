8547e15bafa6a3ec107887a9cc56c4b3b64306ad
Marius Blom <marius.blom@knett.no>
2017-10-23 16:26:06 +0200

    started on task 2

--- assignment01/task02.py
+++ assignment01/task02.py
@@ -20,27 +20,3 @@ plt.figure(2)
 plt.title('Using NumPy array using PIL')
 plt.imshow(img, interpolation='nearest')
 plt.show()
-
-#	transform image using the natural logarithm
-imagelog = image.astype(np.float32)
-c = 255 / np.log(np.max(imagelog) + 1)  # Scaling
-imagelog = c * np.log(imagelog + 1)
-imagelog = imagelog.astype(np.uint8)
-plt.figure()
-plt.imshow(imagelog)
-plt.axis('off')  # Turn of axis numbers and ticks
-plt.show()
-
-#	Plot each channel on its own using subplots
-r = image[..., 0]  # Equivalent to image[:, :, 0]
-g = image[..., 1]
-b = image[..., 2]
-
-_, ax = plt.subplots(1, 3, figsize=(10, 8))
-ax[0].imshow(r, cmap=plt.cm.Reds)  # Red colour map
-ax[0].set_axis_off()
-ax[1].imshow(g, cmap=plt.cm.Greens)  # Green colour map
-ax[1].set_axis_off()
-ax[2].imshow(b, cmap=plt.cm.Blues)  # Blue colour map
-ax[2].set_axis_off()
-plt.show()
