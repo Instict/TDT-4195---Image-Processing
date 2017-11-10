3f16f0231c475899716d9810b64bfc70bee42641
Marius Blom <marius.blom@knett.no>
2017-11-10 18:55:34 +0100

    Finished task 4b

--- assignment02/task04_b.py
+++ assignment02/task04_b.py
@@ -12,24 +12,14 @@ imagePath = {'jellyBeans' : './images/4.1.07-jelly-beans.tiff',
 			'noiseC' : './images/noise-c.tiff',
 			'opera' : './images/opera.tiff'}
 
-image = misc.imread(imagePath['noiseB'])
-image = np.array(image, dtype=np.float32)
 
-spec_orig = np.fft.fft2(image)
-spec_img = np.fft.fftshift(spec_orig)
-misc.imsave('./images/sprecterNoise.tiff', np.log(1+np.abs(spec_img)))
+def spatial2FrequencyDomain(image):
+	# fast fourier transform
+	F_orig = np.fft.fft2(image)
+	# shift image
+	F = np.fft.fftshift(F_orig)
+	return F
 
-width, height = 11, 11
-centerX, centerY = 257, 257
-radius = 52
-epsilon = 20
-
-# draw the circle
-for y in range(100, 400):
-    for x in range(100, 400):
-        # see if we're close to (x-a)**2 + (y-b)**2 == r**2
-        if abs((x-centerX)**2 + (y-centerY)**2 - radius**2) < epsilon**2:
-            spec_img[y][x] = 0
 
 def frequency2SpatialDomain(G):
 	ySize = np.size(G,0)
@@ -42,21 +32,47 @@ def frequency2SpatialDomain(G):
 			g_p[y,x]=g_p[y,x]*np.power(-1,x+y)
 	return g_p
 
-plt.subplot(131)
+
+def filtering(image):
+	width, height = 11, 11
+	centerX, centerY = 257, 257
+	radius = 52
+	epsilon = 19
+	# draw the circle
+	for y in range(100, 400):
+	    for x in range(100, 400):
+	        # see if we're close to (x-a)**2 + (y-b)**2 == r**2
+	        if abs((x-centerX)**2 + (y-centerY)**2 - radius**2) < epsilon**2:
+	            image[y][x] = 0
+	return image
+
+
+image = misc.imread(imagePath['noiseB'])
+image = np.array(image, dtype=np.float32)
+
+# convert to frequency
+F = spatial2FrequencyDomain(image)
+plt.subplot(121)
 plt.axis('off')
-plt.title('original')
-plt.imshow(image, cmap = 'gray')
-plt.subplot(132)
+plt.title('Spectrum of image')
+plt.imshow(np.log(1+np.abs(F)), cmap = 'gray')
+
+# filter image
+filtered = filtering(F)
+plt.subplot(122)
 plt.axis('off')
-plt.title('spectrum')
-plt.imshow(np.log(1+np.abs(spec_img)), cmap = 'gray')
-plt.subplot(133)
-plt.axis('off')
-plt.title('filtered')
-spec_img = frequency2SpatialDomain(spec_img)
-plt.imshow(spec_img, cmap = 'gray')
+plt.title('Spectrum after filter')
+plt.imshow(np.log(1+np.abs(filtered)), cmap = 'gray')
 plt.show()
 
-plt.plot()
-plt.imshow(spec_img, cmap = 'gray')
+# convert to spatial
+g = frequency2SpatialDomain(filtered)
+plt.subplot(121)
+plt.axis('off')
+plt.title('Unfiltered image')
+plt.imshow(image, cmap = 'gray')
+plt.subplot(122)
+plt.axis('off')
+plt.title('Filtered image')
+plt.imshow(g, cmap = 'gray')
 plt.show()
