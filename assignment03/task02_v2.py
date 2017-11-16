aed4d69852a1e53edde38cbfccabe3ad5a34e7b3
Marius Blom <marius.blom@knett.no>
2017-11-16 19:07:58 +0100

    task02_v2

---
+++ assignment03/task02_v2.py
@@ -0,0 +1,14 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy import misc
+from PIL import Image
+
+
+
+
+imagePath = {'yeast' : './images/Fig1043(a)(yeast_USC).tiff',
+			'defective' : './images/Fig1051(a)(defective_weld).tiff',
+			'noisy' : './images/noisy.tiff',
+			'task5one' : './images/task5-01.tiff',
+			'task5two' : './images/task5-02.tiff',
+			'task5three' : './images/task5-03.tiff'}
