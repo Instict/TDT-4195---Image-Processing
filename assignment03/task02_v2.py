15e3125780b57c3669d320c36f171f07453d6da9
Marius Blom <marius.blom@knett.no>
2017-11-16 19:16:22 +0100

    cleanup

--- assignment03/task02_v2.py
+++ assignment03/task02_v2.py
@@ -7,8 +7,17 @@ from PIL import Image
 
 
 imagePath = {'yeast' : './images/Fig1043(a)(yeast_USC).tiff',
-			'defective' : './images/Fig1051(a)(defective_weld).tiff',
+			'weld' : './images/Fig1051(a)(defective_weld).tiff',
 			'noisy' : './images/noisy.tiff',
 			'task5one' : './images/task5-01.tiff',
 			'task5two' : './images/task5-02.tiff',
 			'task5three' : './images/task5-03.tiff'}
+
+image = misc.imread(imagePath['weld'])
+image = np.array(image, dtype=np.float32)
+
+plt.plot()
+plt.axis('off')
+plt.title('Original image')
+plt.imhsow(image, cmap = 'gray')
+plt.show()
