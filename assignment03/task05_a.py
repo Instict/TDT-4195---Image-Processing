c067c98f7020bb20b5114032662c86648b18ff93
Sondre Martin Kvellestad Jensen <Sondre_martin92@hotmail.com>
2017-11-24 18:19:45 +0100

    task05

--- assignment03/task05_a.py
+++ assignment03/task05_a.py
@@ -1,7 +1,7 @@
-import numpy as np
 import matplotlib.pyplot as plt
+import numpy as np
 from scipy import misc
-from PIL import Image
+from scipy import ndimage
 
 #	create a dictionary for easier filepath handling
 imagePath = {'yeast' : './images/Fig1043(a)(yeast_USC).tiff',
@@ -11,96 +11,84 @@ imagePath = {'yeast' : './images/Fig1043(a)(yeast_USC).tiff',
 			'task5two' : './images/task5-02.tiff',
 			'task5three' : './images/task5-03.tiff',
 			'noisless' : './images/noisless.tiff'}
-
-
 			
-def erosion(image, kernel):
+def detectChessColor(image):
 	
-	yImage = np.size(image, 0)
-	xImage = np.size(image, 1)
-
-	erodedImage = np.full((yImage,xImage),0)
-	erodedImage = erodedImage.astype(bool)
+	sum1 = 0
+	sum2 = 0
+	chessColor1 = [0,0,0]
+	chessColor2 = [0,0,0]
 	
-	for y in range(yImage):
-		for x in range(xImage):
-			erodedImage[y,x] = travaserErosion(image, x, y, kernel)
-
-	return erodedImage
-	
-def travaserErosion(image, x, y, kernel):
-	
-	yImage = np.size(image, 0)
-	xImage = np.size(image, 1)
-	
-	sizeKernel = np.size(kernel, 0)
-	centerKernel = sizeKernel//2
-	
-	for j in range(sizeKernel):
-		for i in range(sizeKernel):
-					
-			if(kernel[j,i]):
-					
-				currentPixelY = y - j + centerKernel
-				currentPixelX = x - i + centerKernel
-					
-				if (0 <= currentPixelY < yImage and 0 <= currentPixelX < xImage ):
-					if(image[currentPixelY, currentPixelX] == 0):
-						return 0
-	return 1
-	
-def boundary(image, erodedImage):
-	
-	yImage = np.size(image, 0)
-	xImage = np.size(image, 1)
-	
-	boundaryImage = np.full((yImage,xImage),0)
-	boundaryImage = boundaryImage.astype(bool)
-	
-	for y in range(yImage):
-		for x in range(xImage):
-			if(image[y,x]==erodedImage[y,x]):
-				boundaryImage[y,x]=False
+	for y in range(5):
+		for x in range(7):
+			if (image[5+100*y,50+100*x,0]<100):
+				chessColor1 += image[5+100*y,50+100*x,:]
+				sum1+=1
 			else:
-				boundaryImage[y,x]=True
-			
-	return boundaryImage
+				chessColor2 += image[5+100*y,50+100*x,:]
+				sum2+=1
+	
+	chessColor1 = chessColor1//sum1
+	chessColor2 = chessColor2//sum2
+	
+	print('Collor1: ',chessColor1)
+	print('Collor2: ',chessColor2)
+
+	return (chessColor1, chessColor2)
+	
+def removeChessBoard(image, color1, color2):
+	
+	yImage = np.size(image, 0)
+	xImage = np.size(image, 1)
+	
+	print(image.shape)
+	
+	canvas = np.full((yImage,xImage),1)
+	canvas = canvas.astype(bool)
+	
+	for y in range(yImage):
+		for x in range(xImage):
+			canvas[y,x] = not(imageTreshold(image, y, x, color1) or imageTreshold(image, y, x, color2) )
+	
+	return canvas
+	
+def imageTreshold(image, y, x, color):
+	
+	diff = np.absolute(image[y,x] - color)
+	treshold = 66
+	
+	magnetude = np.sqrt(np.power(diff[0],2)+np.power(diff[1],2)+np.power(diff[2],2))
+	#print(magnetude)
+	return magnetude < treshold
+		
+def removingNoise(image):
+	
+	kernel = np.array([[1, 1, 1],
+						[1, 1, 1],
+						[1, 1, 1]
+						])
+	
+	canvas = ndimage.binary_opening(image, kernel, iterations=3)
+	canvas = ndimage.binary_closing(canvas, kernel, iterations=1)
 	
 	
-	
-	
-kernel =  np.array([
-  [1, 1, 1],
-  [1, 1, 1],
-  [1, 1, 1]
-])	
-	
-image = misc.imread(imagePath['noisless'])
-image = np.array(image, dtype=np.float32)
-print(image.shape)
+	return canvas
 
+image = misc.imread(imagePath['task5three'])
+originalImage = image
 
-threshold = 127
+(color1, color2) = detectChessColor(image)
 
-binaryImage = image
-binaryImage = binaryImage.astype(bool)
+binaryImage = removeChessBoard(image, color1, color2)
+binaryImage = removingNoise(binaryImage)
 
-erodedImage = erosion(binaryImage, kernel)
-
-boundaryImage = boundary(binaryImage, erodedImage)
-
-plt.subplot(131)
+plt.subplot(121)
 plt.axis('off')
 plt.title('Original image')
+plt.imshow(originalImage)
+plt.subplot(122)
+plt.axis('off')
+plt.title('Binary image')
 plt.imshow(binaryImage, cmap = 'gray',  interpolation='nearest')
-plt.subplot(132)
-plt.axis('off')
-plt.title('Eroded image')
-plt.imshow(erodedImage, cmap = 'gray',  interpolation='nearest')
-plt.subplot(133)
-plt.axis('off')
-plt.title('boundary image')
-plt.imshow(boundaryImage, cmap = 'gray',  interpolation='nearest')
 
 plt.show()
-
